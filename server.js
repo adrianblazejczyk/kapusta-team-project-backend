64d0c20dc12a27ab065399ff7bc80536e24da8c8
ArturBaur <artur.baur@gmail.com>
2024-03-13 18:47:59 +0100

    Add dotenv config and MongoDB connection in server.js
    This commit adds dotenv configuration to load environment variables and connect to MongoDB in server.js for better configuration management and database integration

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "./config/.env" });
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_URL = process.env.DB_HOST;
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(DB_URL);
+
+mongoose.connection.on("connected", () => {
+  console.log("Database connection successful");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Database connection error:", err);
+  process.exit(1);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Database disconnected. Trying to reconnect...");
+  mongoose.connect(DB_URL);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
