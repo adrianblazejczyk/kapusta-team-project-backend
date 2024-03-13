27fa1d150882f862f09785cf9c00deae09a456ee
ArturBaur <artur.baur@gmail.com>
2024-03-14 00:19:36 +0100

    Fixed express.urlencoded(), added path module import, and set path for static file serving
    Updated app.js to utilize express.urlencoded({ extended: true }), included path module for file serving, and set up static file serving path

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require("dotenv").config();
+require("dotenv").config({ path: "./config/.env" });
 const logger = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -9,21 +9,25 @@ const DB_URL = process.env.DB_HOST;
 
 const db = mongoose.connect(DB_URL);
 
+const signupRouter = require("./src/routes/signup");
+
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/signup", signupRouter);
+
 app.use((req, res) => {
-    res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-    res.status(500).json({ message: err.message });
+  res.status(500).json({ message: err.message });
 });
 module.exports = { app, db };
