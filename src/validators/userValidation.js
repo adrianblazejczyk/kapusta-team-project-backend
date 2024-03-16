2cae0acfdc16cbee734cb8f7adabf6b3b95c4bab
ArturBaur <artur.baur@gmail.com>
2024-03-16 01:19:11 +0100

    No actual changes were made, only lines were rearranged.

--- src/validators/userValidation.js
+++ src/validators/userValidation.js
@@ -3,27 +3,27 @@ const Joi = require("joi");
 // const bCrypt = require("bcryptjs");
 
 const newUserAuthSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(8).max(30).required(),
-    // token: Joi.string(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).max(30).required(),
+  // token: Joi.string(),
 });
 
 const loginUserAuthSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(8).max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).max(30).required(),
 });
 
 const currUserAuthSchema = Joi.object({
-    token: Joi.string().required(),
+  token: Joi.string().required(),
 });
 
 const checkUserEmailSchema = Joi.object({
-    email: Joi.string().email().required(),
+  email: Joi.string().email().required(),
 });
 
 module.exports = {
-    newUserAuthSchema,
-    loginUserAuthSchema,
-    currUserAuthSchema,
-    checkUserEmailSchema,
+  newUserAuthSchema,
+  loginUserAuthSchema,
+  currUserAuthSchema,
+  checkUserEmailSchema,
 };
