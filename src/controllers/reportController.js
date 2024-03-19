7e5bc1a4113b3e3d343c2d4fe994f33f8b3f2289
ArturBaur <artur.baur@gmail.com>
2024-03-19 01:06:58 +0100

    Refactor expenses route handling
    Refactored the handling of the expenses route (getExpensesReport) in the controller to simplify the code and use req.user.id to retrieve the user ID instead of the query. The changes aim to improve readability and consistency in the code.

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -1,40 +1,32 @@
 const reportService = require("../services/reportService");
 
 const getExpensesReport = async (req, res, next) => {
-    try {
-        const { userId, year } = req.query;
-
-        if (!userId || !year) {
-            return res
-                .status(400)
-                .json({ message: "User ID and year are required" });
-        }
-
-        const expensesReport = await reportService.getExpensesReport(
-            userId,
-            year
-        );
-
-        res.json(expensesReport);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const expensesReport = await reportService.getExpensesReport(req.user.id);
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      expensesReport,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getIncomeReport = async (req, res, next) => {
-    try {
-        const report = await reportService.getIncomeReport(req.user.id);
-        res.status(200).json({
-            status: "success",
-            code: 200,
-            report,
-        });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const report = await reportService.getIncomeReport(req.user.id);
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      report,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
-    getExpensesReport,
-    getIncomeReport,
+  getExpensesReport,
+  getIncomeReport,
 };
