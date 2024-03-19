f9c82f0752a86b4c900a2e883a1dd617bc70528d
ArturBaur <artur.baur@gmail.com>
2024-03-19 01:08:28 +0100

    Simplify expenses report generation
    Simplified the logic for generating expenses reports by removing unnecessary date range filtering and refactoring to calculate monthly sums directly from transactions. This change aims to improve code readability and maintainability.

--- src/services/reportService.js
+++ src/services/reportService.js
@@ -1,73 +1,57 @@
 const Transaction = require("../schemas/transactions");
 
-const getExpensesReport = async (userId, year) => {
-    const expensesReport = {};
+const getExpensesReport = async (userId) => {
+  try {
+    const allTransactions = await Transaction.find({
+      user: userId,
+      type: "Expenses",
+    });
+    const monthlySum = {};
 
-    try {
-        const startOfYear = new Date(year, 0, 1);
-        const endOfYear = new Date(year, 11, 31);
+    allTransactions.forEach((item) => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const key = `${year}-${month}`;
 
-        const transactions = await Transaction.find({
-            user: userId,
-            type: "Expenses",
-            date: {
-                $gte: startOfYear,
-                $lte: endOfYear,
-            },
-        });
-
-        if (transactions.length === 0) {
-            throw new Error(
-                "No expenses transactions found for the given user and year"
-            );
-        }
-
-        transactions.forEach((transaction) => {
-            const month = transaction.date.getMonth();
-            const monthName = new Date(year, month).toLocaleString("default", {
-                month: "long",
-            });
-
-            if (!expensesReport[monthName]) {
-                expensesReport[monthName] = 0;
-            }
-
-            expensesReport[monthName] += transaction.amount;
-        });
-
-        return expensesReport;
-    } catch (error) {
-        console.error("Error while generating expenses report:", error);
-        throw new Error("Error while generating expenses report");
-    }
+      if (monthlySum[key]) {
+        monthlySum[key] += item.amount;
+      } else {
+        monthlySum[key] = item.amount;
+      }
+    });
+    return monthlySum;
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 const getIncomeReport = async (userId) => {
-    try {
-        const allTransactions = await Transaction.find({
-            user: userId,
-            type: "Income",
-        });
-        const monthlySum = {};
+  try {
+    const allTransactions = await Transaction.find({
+      user: userId,
+      type: "Income",
+    });
+    const monthlySum = {};
 
-        allTransactions.forEach((item) => {
-            const date = new Date(item.date);
-            const year = date.getFullYear();
-            const month = date.getMonth() + 1;
-            const key = `${year}-${month}`;
+    allTransactions.forEach((item) => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const key = `${year}-${month}`;
 
-            if (monthlySum[key]) {
-                monthlySum[key] += item.amount;
-            } else {
-                monthlySum[key] = item.amount;
-            }
-        });
-        return monthlySum;
-    } catch (err) {
-        console.log(err.message);
-    }
+      if (monthlySum[key]) {
+        monthlySum[key] += item.amount;
+      } else {
+        monthlySum[key] = item.amount;
+      }
+    });
+    return monthlySum;
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 module.exports = {
-    getExpensesReport,
-    getIncomeReport,
+  getExpensesReport,
+  getIncomeReport,
 };
