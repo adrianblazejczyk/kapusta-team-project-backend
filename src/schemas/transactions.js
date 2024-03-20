5ea6b93f6db9bac94f8bd4144962c76ca9a72ed5
ArturBaur <artur.baur@gmail.com>
2024-03-20 21:01:20 +0100

    Added 'description' field to the transaction schema

--- src/schemas/transactions.js
+++ src/schemas/transactions.js
@@ -1,31 +1,34 @@
 const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema(
-    {
-        date: {
-            type: Date,
-            required: true,
-        },
-        type: {
-            type: String,
-            required: true,
-            enum: ["Income", "Expenses"],
-        },
-        category: {
-            type: String,
-            required: true,
-        },
-        amount: {
-            type: Number,
-            required: true,
-        },
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
+  {
+    date: {
+      type: Date,
+      required: true,
     },
-    { versionKey: false }
+    type: {
+      type: String,
+      required: true,
+      enum: ["Income", "Expenses"],
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { versionKey: false }
 );
 
 const Transaction = mongoose.model("transaction", transactionSchema);
