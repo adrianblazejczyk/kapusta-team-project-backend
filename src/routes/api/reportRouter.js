71b9935c030d53f3f23b55c8f8cd34d0262258b6
ArturBaur <artur.baur@gmail.com>
2024-03-20 21:00:01 +0100

    Added routes for detailed report and detailed category repor
    Implemented additional routes in reportRouter.js to handle requests for detailed report and detailed category report, utilizing corresponding controller functions

--- src/routes/api/reportRouter.js
+++ src/routes/api/reportRouter.js
@@ -4,6 +4,12 @@ const router = express.Router();
 const reportController = require("../../controllers/reportController");
 const authMiddleware = require("../../middlewares/verifyToken");
 
+router.get("/detailed", authMiddleware, reportController.getDetailedReport);
+router.get(
+  "/detailed/category",
+  authMiddleware,
+  reportController.getDetailedCategoryReport
+);
 router.get("/expenses", authMiddleware, reportController.getExpensesReport);
 router.get("/income", authMiddleware, reportController.getIncomeReport);
 module.exports = router;
