5883c5045286c8204833d6c5978d751612ecaff2
RadekKisiela <radek1997_97@o2.pl>
2024-03-22 22:48:29 +0100

    Added googleLogin to router

--- src/routes/api/usersRouter.js
+++ src/routes/api/usersRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const {
-    signup,
-    login,
-    logout,
-    currentUser,
-    updateUserBalance,
+  signup,
+  login,
+  logout,
+  googleLogin,
+  currentUser,
+  updateUserBalance,
 } = require("../../controllers/userController");
 const verifyToken = require("../../middlewares/verifyToken");
 
@@ -12,6 +13,7 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/googleLogin", googleLogin);
 router.get("/logout", verifyToken, logout);
 router.get("/current", verifyToken, currentUser);
 router.patch("/balance", verifyToken, updateUserBalance);
