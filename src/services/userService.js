7f88d0018d1d086052f51dd5e7ff360c82e332b5
RadekKisiela <radek1997_97@o2.pl>
2024-03-22 22:50:18 +0100

    Created findUserByGoogleId and createUser

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,32 +1,44 @@
 const User = require("../schemas/user");
 
 async function addNewUser(userData, token) {
-    return await User.create(userData);
+  return await User.create(userData);
 }
 
 async function findUserByEmail(email) {
-    return await User.findOne({ email: email });
+  return await User.findOne({ email: email });
 }
 
 async function findUserById(id) {
-    return await User.findById(id);
+  return await User.findById(id);
 }
 
 async function updateUserToken(id, token) {
-    return await User.findByIdAndUpdate(id, { token: token });
+  return await User.findByIdAndUpdate(id, { token: token });
 }
 
 const updateUsersBalance = async (userId, updateData) => {
-    const user = await User.findByIdAndUpdate(userId, updateData, {
-        new: true,
-    });
-    return user;
+  const user = await User.findByIdAndUpdate(userId, updateData, {
+    new: true,
+  });
+  return user;
 };
 
+async function findUserByGoogleId({ sub: googleId }) {
+  return await User.findOne({ googleId });
+}
+
+async function createUser({ sub: googleId, email }) {
+  const user = new User({ googleId, email });
+  await user.save();
+  return user;
+}
+
 module.exports = {
-    addNewUser,
-    findUserByEmail,
-    findUserById,
-    updateUserToken,
-    updateUsersBalance,
+  addNewUser,
+  findUserByEmail,
+  findUserById,
+  updateUserToken,
+  updateUsersBalance,
+  findUserByGoogleId,
+  createUser,
 };
