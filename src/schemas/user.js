9a8bd2ff9731ffd18905c0b23a420e31ea57dab6
RadekKisiela <radek1997_97@o2.pl>
2024-03-22 22:49:02 +0100

    Update userSchema

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -3,35 +3,44 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    password: {
-        type: String,
-        required: [true, "Password is required"],
+  password: {
+    type: String,
+    validate: {
+      validator: function (value) {
+        // Check if either password is provided or googleId is provided
+        return value || this.googleId;
+      },
+      message: "Password is required if googleId is not provided",
     },
-    email: {
-        type: String,
-        required: [true, "Email is required"],
-        unique: true,
-    },
-    balance: {
-        type: Number,
-        default: 0,
-    },
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  balance: {
+    type: Number,
+    default: 0,
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+  googleId: {
+    type: String,
+    default: null,
+  },
+  // FOR PASSWORD USAGE IN THE FUTURE
+  // verify: {
+  //     type: Boolean,
+  //     default: false,
+  // },
+  // verificationToken: {
+  //     type: String,
+  //     required: [true, "Verify token is required"],
+  // },
 
-    token: {
-        type: String,
-        default: null,
-    },
-    // FOR PASSWORD USAGE IN THE FUTURE
-    // verify: {
-    //     type: Boolean,
-    //     default: false,
-    // },
-    // verificationToken: {
-    //     type: String,
-    //     required: [true, "Verify token is required"],
-    // },
-
-    //
+  //
 });
 // FOR PASSWORD USAGE IN THE FUTURE
 // userSchema.methods.setPassword = function (password) {
