9c0e4c99eeedc0fdf0276c7b01312d19c54c3242
ArturBaur <artur.baur@gmail.com>
2024-03-24 11:34:54 +0100

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,32 +1,32 @@
 const User = require("../schemas/user");
 
-async function addNewUser(userData, token) {
-    return await User.create(userData);
+async function addNewUser(userData) {
+  return await User.create(userData);
 }
 
 async function findUserByEmail(email) {
-    return await User.findOne({ email: email });
+  return await User.findOne({ email: email });
 }
 
 async function findUserById(id) {
-    return await User.findById(id);
+  return await User.findById(id);
 }
 
 async function updateUserToken(id, token) {
-    return await User.findByIdAndUpdate(id, { token: token });
+  return await User.findByIdAndUpdate(id, { token: token });
 }
 
 const updateUsersBalance = async (userId, updateData) => {
-    const user = await User.findByIdAndUpdate(userId, updateData, {
-        new: true,
-    });
-    return user;
+  const user = await User.findByIdAndUpdate(userId, updateData, {
+    new: true,
+  });
+  return user;
 };
 
 module.exports = {
-    addNewUser,
-    findUserByEmail,
-    findUserById,
-    updateUserToken,
-    updateUsersBalance,
+  addNewUser,
+  findUserByEmail,
+  findUserById,
+  updateUserToken,
+  updateUsersBalance,
 };
